32fe679c2a5f491a10ce50814dc2d75922a3eee7
Navneet <navneetjh5@gmail.com>
2020-10-04 10:39:08 +0530

    Board contour created

--- src/com/cg/tictactoegame/TicTacToeGame.java
+++ src/com/cg/tictactoegame/TicTacToeGame.java
@@ -9,7 +9,7 @@ public class TicTacToeGame {
 
 		System.out.println("WELCOME TO TIC-TAC-TOE");
 		assignBlankSpace(board);
-		
+		showBoard(board);
 		System.out.println("Player:Enter the choice(X | O)");
 		char player = Character.toUpperCase(input.next().charAt(0));
 		char computer = (player == 'X' ? 'O' : 'X');
@@ -22,5 +22,14 @@ public class TicTacToeGame {
 			board[i] = ' ';
 
 	}
+	
+	public static void showBoard(char[] board) {
+		System.out.println("\tBOARD FORMAT");
+		System.out.println("\t  " + board[1] + "| " + board[2] + " | " + board[3] + "\n\t-----------");
+		System.out.println("\t  " + board[4] + "| " + board[5] + " | " + board[6] + "\n\t-----------");
+		System.out.println("\t  " + board[7] + "| " + board[8] + " | " + board[9]);
+	}
+
+
 
 }
